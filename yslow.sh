9d5c5102cfee7c367e8ced4d0934c380ac3c54d0
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-10-27 23:25:08 -0400

    Moved logfile destination to the top

--- yslow.sh
+++ yslow.sh
@@ -2,6 +2,13 @@
 
 SHOWSLOWBASE='http://www.showslow.com' # e.g. 'http://www.showslow.com'
 
+# Download and install PhantomJS: http://phantomjs.org/download.html
+PHANTOMJS='/home/ec2-user/phantomjs/bin/phantomjs'
+
+# Download the latest build of phantomjs version of yslow: https://github.com/marcelduran/yslow/downloads
+YSLOWJS='/home/ec2-user/user-repo/yslow.js'
+YSLOWLOG='/home/ec2-user/user-logs/yslow.log'
+
 # Resetting the log
 >$YSLOWLOG
 
@@ -12,13 +19,6 @@ else
 	echo "Testing all URLs" >>$YSLOWLOG
 	URLS=`curl -s $SHOWSLOWBASE/monitor.php`
 fi
-
-# Download and install PhantomJS: http://phantomjs.org/download.html
-PHANTOMJS='/home/ec2-user/phantomjs/bin/phantomjs'
-
-# Download the latest build of phantomjs version of yslow: https://github.com/marcelduran/yslow/downloads
-YSLOWJS='/home/ec2-user/user-repo/yslow.js'
-YSLOWLOG='/home/ec2-user/user-logs/yslow.log'
 
 mkdir -p /home/ec2-user/user-logs/process.cache/
 
