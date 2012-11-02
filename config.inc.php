af4b1fa0e9e3477f5d95d97b671e68ba5ad842d7
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-11-02 16:36:57 -0400

    Fixed file name

--- config.inc.php
+++ config.inc.php
@@ -12,7 +12,7 @@ $queueName = 'ShowSlowTests';
 
 define('SHOWSLOW_BASE', 'http://www.showslow.com');
 define('TEMP_FOLDER', '/home/user/user-logs');
-define('QUEUE_URL_CACHE', TEMP_FOLDER . '.queueURLCache');
+define('QUEUE_URL_CACHE', TEMP_FOLDER . '/.queueURLCache');
 
 // time during which we expect the result to be produced for each chunk
 define('DEFAULT_VISIBILITY_TIMEOUT', 9000);
