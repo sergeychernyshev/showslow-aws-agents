96804b4c58bcdf852c35cf40abb2627d11924879
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-11-02 18:31:28 -0400

    AWS Keys debug info

--- config.inc.php
+++ config.inc.php
@@ -8,6 +8,9 @@ $system_ini = parse_ini_file("s/home/ec2-user/system.ini", true);
 define('SQS_AWS_ACCESS_KEY_ID', $system_ini['[aws]']['aws-key']);
 define('SQS_AWS_SECRET_ACCESS_KEY', $system_ini['[aws]']['aws-secret']);
 
+echo "AWS Key: ".SQS_AWS_ACCESS_KEY_ID;
+echo "AWS Secret: ".SQS_AWS_SECRET_ACCESS_KEY;
+
 /////////////////// SET PARAMETERS HERE /////////////////////
 $queueName = 'ShowSlowTests';
 
